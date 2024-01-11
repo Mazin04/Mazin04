68d8e83c5897b4f297414e04eb0a8f466ce64e3c
Ruben <rubengsegoviano@gmail.com>
2024-01-11 19:57:10 +0100

    Footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+export function Footer() {
+    return (
+        <footer className="footer border z-10 border-t-[#ffffff] border-l-transparent border-r-transparent border-b-transparent text-white">
+            <div className="container p-12 flex justify-between">
+                <span>This portfolio is a work-in-progress</span>
+                <p className="text-white">Rubén García</p>
+            </div>
+        </footer>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom/client'
 import { Navbar } from './components/navbar'
 import { Hero } from './components/hero'
 import { About } from './components/about'
+import { Footer } from './components/footer'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
-    <section id='Top' className="py-20 h-max">
+    <section id='Top' className="pt-20 h-max">
       <main className="flex h-max flex-col">
         <div className="container mt-16 mx-auto px-2">
           <Hero />
         </div>
-        <About />
+        <div className='py-16'>
+          <About />
+        </div>
       </main>
+      <Footer/>
     </section>
   </React.StrictMode>,
 )
